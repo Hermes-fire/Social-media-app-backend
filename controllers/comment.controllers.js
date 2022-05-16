92a70b4af27f10af1e735faee1dc18af8895918b
Hermes-fire <amine.sadali@gmail.com>
2022-05-16 17:51:31 +0100

    working on reactions

--- controllers/comment.controllers.js
+++ controllers/comment.controllers.js
@@ -101,5 +101,3 @@ exports.removeComment = async (req, res) => {
   });
   }
 }
-
-
--- controllers/reaction.controllers.js
+++ controllers/reaction.controllers.js
@@ -32,3 +32,71 @@ exports.addReaction = async (req, res) => {
     });
   }
 }
+
+
+
+exports.getReactionById = (req, res, next, id) => {
+  Comment.findById(id)
+    .exec((err, reaction) => {
+      if(err || !reaction) {
+          return res.status(400).json({
+              error: 'reaction not found'
+          })
+      }
+      req.reaction = reaction 
+      next()
+  })
+}
+
+exports.readReaction = (req, res) => {
+  return res.json(req.reaction);
+};
+
+
+exports.updateReaction = async (req, res) => {
+  const reaction = new Reaction(req.reaction);
+  if(!req.body.reactionType){
+    return res.status(400).json({
+      error: 'please specify a reaction',
+      });
+  }
+  //check if user own comment
+  if(req.id != req.comment.userId){
+    return res.status(400).json({
+      error: 'unauthorized',
+      });
+  }
+  try{
+    await comment.updateOne({comment: req.body.comment})
+    return res.json({
+      msg: "updated"
+    })
+  } catch(err) {
+  return res.status(400).json({
+  error: err,
+  });
+}}
+
+exports.removeComment = async (req, res) => {
+  if(req.id != req.comment.userId){
+    return res.status(400).json({
+      error: 'unauthorized',
+      });
+  }
+  try{
+    const announcement = await Announcement.findOneAndUpdate(
+      {_id: req.comment.postId}, //filter
+      { $pull: {comments:req.comment._id}}, //update
+      {new: true} //option
+    )
+    await Comment.remove({ _id: req.comment._id})
+    return res.json({
+      msg: "removed",
+      announcement: announcement
+    })
+  } catch(err) {
+  return res.status(400).json({
+  error: err,
+  });
+  }
+}
